4fa4e30e9383289ece2ce449c0c7830e5a409add
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-11-03 12:27:30 -0300

    ok

--- src/app/admin/disponibilidade/page.tsx
+++ src/app/admin/disponibilidade/page.tsx
@@ -3,10 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import AdminPrivateRoute from '@/components/AdminPrivateRoute';
 import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import styles from '@/styles/Admin.module.css';
